e974572217114240a1cc308fa93a721c55a09d85
James <DarkHeart527@users.noreply.github.com>
2018-03-08 11:13:15 -0800

    Update slash.js

--- slash.js
+++ slash.js
@@ -13,8 +13,18 @@
 
 
 
-
+https://stackoverflow.com/questions/9496427/get-elements-by-attribute-when-queryselectorall-is-not-available-without-using-l
 */
+
+Element.prototype.getElementsByAttribute = function(attr) {
+ let found = [];
+ var els = document.getElementsByTagName('*');
+ for (let i = 0; i < els.length; i++) {
+  if (els[i].attributes.slash) {
+   found[found.length] = els[i];
+  }
+ }
+}
 
 let slash = (func) => {
  let type = undefined;
@@ -37,7 +47,7 @@ let slash = (func) => {
    // Find all commands and update
   break;
   case 'exe':
-   
+   var els = document.getElementsByClassName('slash');
   break;
   default:
     console.error(`function not found @ ${func}()`);
