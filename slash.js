8eaf44ef787fef4b2f0904c8d216340802947db3
James <DarkHeart527@users.noreply.github.com>
2018-03-08 11:02:18 -0800

    Update slash.js

--- slash.js
+++ slash.js
@@ -22,9 +22,21 @@ let slash = (func) => {
   type = '//';
  } else if (func[0] == '/' && func[1] != '/') {
   type = '/';
+ } else {
+  type = func;
  }
  switch(type) {
   case '/':
+   // Call Function
+  break;
+  case '//':
+   // Edit Function
+   
+  break;
+  case 'update':
+   // Find all commands and update
+  break;
+  case 'exe':
    
   break;
   default:
